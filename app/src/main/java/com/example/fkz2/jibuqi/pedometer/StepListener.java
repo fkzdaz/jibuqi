db0bd245913f722597c52fe9ba4ed0d83407f5a8
fkzdaz <1247809987@qq.com>
2017-05-19 10:29:01 +0000

    Initial commit

---
+++ app/src/main/java/com/example/fkz2/jibuqi/pedometer/StepListener.java
@@ -0,0 +1,4 @@
+package com.example.fkz2.jibuqi.pedometer;
+public interface StepListener {
+    public void onStep();
+}
---
+++ app/src/main/java/com/example/fkz2/jibuqi/step/config/Constant.java
@@ -0,0 +1,7 @@
+package com.example.fkz2.jibuqi.step.config;
+
+public class Constant {
+    public static final int MSG_FROM_CLIENT = 0;
+    public static final int MSG_FROM_SERVER = 1;
+    public static final int REQUEST_SERVER = 2;
+}
